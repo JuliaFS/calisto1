de0b710eb0dd936c8fc53e22428c9a2600d4d8dd
Yulia <yuliya.f.s@gmail.com>
2024-04-16 12:06:41 +0300

    generate project angular 16

---
+++ calisto1/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyA8t2HLUHPWs9TivNoRO1_DDafPYRaG5gE",
+        authDomain: "calisto-b369b.firebaseapp.com",
+        projectId: "calisto-b369b",
+        storageBucket: "calisto-b369b.appspot.com",
+        messagingSenderId: "831805876742",
+        appId: "1:831805876742:web:9f97df9dcb8609e54a7187"
+      },
+      production: false
+};
